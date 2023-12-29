f0f5075f1257b917b63cbf153642b0aeb6f9f994
Anton <a.tashchilin@bk.ru>
2023-12-29 08:53:47 +0400

    delete dep

--- src/test/java/com/charity/activism/ActivismAndCharityApplicationTests.java
+++ src/test/java/com/charity/activism/ActivismAndCharityApplicationTests.java
@@ -3,12 +3,12 @@ package com.charity.activism;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.charity.activism.controllers.ActivismController;
+
 
 @SpringBootTest
 class ActivismAndCharityApplicationTests {
 
-	ActivismController activismController;
+	
 
 	@Test
 	void contextLoads() {
